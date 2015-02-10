5da8a6c7ccfe4cc220909ec62fe206b053ab658f
yuanzai <junyuan.lau@gmail.com>
2015-02-10 17:16:57 +0800

    Update urls.py

--- voucr/urls.py
+++ voucr/urls.py
@@ -2,11 +2,13 @@ import sys
 sys.path.append('/home/ec2-user/voucr')
 
 from django.conf.urls import patterns, include, url
+import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'voucr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^$', 'views.index'),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login/template/login.html'}),
 )
