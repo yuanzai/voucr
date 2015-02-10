63d4daaa25c7338819f23f6d6c2db819784c3ca1
yuanzai <junyuan.lau@gmail.com>
2015-02-10 17:40:09 +0800

    Update views.py

---
+++ voucr/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, get_object_or_404
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+            if form.is_valid():
+                # Do something with the data
+                pass
+    else:
+        form = UserCreationForm()
+        return render(request, 'operations/tester.html',{'form':form})
