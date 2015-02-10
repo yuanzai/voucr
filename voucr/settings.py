54aec76bc3a830457db90e44b02aa29bcf26146c
yuanzai <junyuan.lau@gmail.com>
2015-02-10 10:31:09 +0800

    RDS link
    updated db details

--- voucr/settings.py
+++ voucr/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'voucr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'voucrdb',
+        'USER': 'voucr',
+        'PASSWORD': 'voucr123',
+        'HOST': 'voucrdb.c1hpszgpu6nt.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
