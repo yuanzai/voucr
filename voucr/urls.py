2d80954dfd1ddd6ec1475e149356b680043d9864
yuanzai <junyuan.lau@gmail.com>
2015-02-11 11:37:21 +0800

    Update urls.py

--- voucr/urls.py
+++ voucr/urls.py
@@ -2,11 +2,12 @@ import sys
 #sys.path.append('/home/ec2-user/voucr')
 
 from django.conf.urls import patterns, include, url
-from voucr.views import signup, login, index
+from voucr.views import signup, login, index, logout
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', index),
     url(r'^login/$', login),
+    url(r'^logout/$', logout),
     url(r'^signup/$', signup),
 )
