d4df026774b6dcc43523417e95cd54706085c19f
yuanzai <junyuan.lau@gmail.com>
2015-02-12 14:27:39 +0800

    Update models.py

--- voucr/models.py
+++ voucr/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, primary_key=true)
     longname = models.CharField(max_length=255)
     urlshortname = models.CharField(max_length=20, db_index=true)
 
+class UserInfoForm(ModelForm):
+    class meta:
+        model = UserInfo
+        field = ['longname','urlshortname']
+
 class Campaign(models.Model)
     user = models.ForeignKey(UserInfo)
     desc_url = models.CharField(max_length=40)
     desc = models.CharField(max_length=255)
+
+class CampaignForm(ModelForm):
+    class meta:
+        model = Campaign
+        field = ['desc_url','desc']
 
 class Voucher(models.Model)
     campaign = models.ForeignKey(Campaign)
@@ -18,5 +29,8 @@ class Voucher(models.Model)
     date_url = models.CharField(max_length=7, db_index=true)
     expire_date = models.DateField()
     
-    
+class VoucherForm(ModelForm):
+    class meta:
+        model = Voucher
+        field = ['char_url','word_url','expire_date']    
     
