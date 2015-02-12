79209ae7624bb651d0232fa9f4959b0023cf6b0e
Cloud User <ec2-user@ip-172-31-19-213.ap-southeast-1.compute.internal>
2015-02-11 21:25:42 -0500

    models in

--- voucr/models.py
+++ voucr/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+class UserInfo(models.Model):
+    shortname = models.CharField(max_length=20, db_index=true)
+    user = models.OneToOneField(User, primary_key=true)
+    longname = models.CharField(max_length=255)
+
 
 class Campagin(modes
