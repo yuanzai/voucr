47db7444b4302ed83b53ff82b52b8c333f1acea4
yuanzai <junyuan.lau@gmail.com>
2015-02-18 13:54:07 +0800

    Update link_gen.py

--- voucr/link_gen.py
+++ voucr/link_gen.py
@@ -1,4 +1,15 @@
 from models import Voucher, Campaign
+import string
+import random
 
 def link_generator(campaign):
-    pass
+    char_list = string.ascii_letters + string.digits
+    for i in range(campaign.count):
+        while (true):
+            new_char_url = ''.join(random.choice(char_list) for j in range(12))
+            try:
+                Voucher.objects.get(char_url=new_char_url)
+            except:
+                v = Voucher(campaign=campaign,char_url=new_char_url)
+                v.save()
+                break
