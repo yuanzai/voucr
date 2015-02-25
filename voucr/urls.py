e8d7000b24897afcd1a939bbfb67c000e9f0a98b
yuanzai <junyuan.lau@gmail.com>
2015-02-25 17:31:18 +0800

    Update urls.py

--- voucr/urls.py
+++ voucr/urls.py
@@ -2,19 +2,24 @@ import sys
 #sys.path.append('/home/ec2-user/voucr')
 
 from django.conf.urls import patterns, include, url
+from django.contrib.auth import views as auth_views
+
 from voucr.views import campaign_create, user_home, user_create, signup_page, login_page, index, logout_page, get_voucher, get_voucher_word
 import voucr.views
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', index),
-    url(r'^login$', login_page),
-    url(r'^logout$', logout_page),
-    url(r'^signup/$', signup_page),
-    url(r'^signup$', signup_page),
+    #url(r'^login$', login_page),
+    #url(r'^logout$', logout_page),
+    url(r'^login$', auth_views.login, {'template_name': 'login.html'}),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}),
     
-    url(r'^user_create/$', user_create),
-    url(r'^campaign_create$', campaign_create),
+    url(r'^signup/$', voucr.views.signup_page),
+    url(r'^signup$', voucr.views.signup_page),
+    
+    url(r'^user_create/$', voucr.views.user_create),
+    url(r'^campaign_create$', voucr.views.campaign_create),
     url(r'^campaign$', voucr.views.campaign_home),
     
     url(r'^campaign/(?P<campaign_id>\d+)$', voucr.views.campaign_view),
