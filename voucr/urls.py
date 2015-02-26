e4d7201582a3a770224c761d5cc9b8c8180d81c0
yuanzai <junyuan.lau@gmail.com>
2015-02-26 10:17:57 +0800

    Update urls.py

--- voucr/urls.py
+++ voucr/urls.py
@@ -2,7 +2,7 @@ import sys
 #sys.path.append('/home/ec2-user/voucr')
 
 from django.conf.urls import patterns, include, url
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth import views as auth_views
 
 from voucr.views import campaign_create, user_home, user_create, signup_page, login_page, index, logout_page, get_voucher, get_voucher_word
 import voucr.views
@@ -10,9 +10,9 @@ import voucr.views
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', index),
-    #url(r'^login$', login_page),
-    #url(r'^logout$', logout_page),
-    url(r'^login?//$', auth_views.login, {'template_name': 'login.html'},name='login'),
+    url(r'^login?//$', login_page),
+    url(r'^logout$', logout_page),
+    #url(r'^login?//$', auth_views.login, {'template_name': 'login.html'},name='login'),
     #url(r'^login/$', auth_views.login, {'template_name': 'login.html'},name='login'),
     
     url(r'^signup/$', voucr.views.signup_page),
